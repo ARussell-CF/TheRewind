7ef972de9d15cbed96429b0bddf0b32b9386f4ef
Andrew Russell <67603030+ARussell-CF@users.noreply.github.com>
2021-10-05 13:18:56 -0400

    Basic build

--- js/site.js
+++ js/site.js
@@ -1,17 +1,43 @@
 //get starting values from the screen
 //Controller Function
-function getValues() {
-    Alert("Hello App");
+function getString() {
+    //Get the user's input
+    let userString = document.getElementById("userString").value;
+    //Version 2 - Test the user's input for length
+
+    //Reverse the user's input - this is a separate function
+    let revString = reverseString(userString);
+
+    //Display the result - this is a separate function
+    displayString(revString);
 }
 
-function getValues2() {
-    let msg = "";
+function reverseString(userString) {
+    let startValue = userString.length - 1;
+    let endValue = 0;
 
-    msg = document.getElementById("message").value;
+    //User input = Frank
+    //Frank is a string AND an array of characters
+    //[F, r, a, n, k]
 
-    Swal.fire(
-        'APP NAME',
-        msg,
-        'success'
-    )
+    let revString = "";
+
+    //In a decremented for loop the startValue must be higher than the endValue
+    for (let i = startValue; i >= endValue; i--) {
+        revString += userString[i];
+    };
+
+    //Loop 1 - "k"
+    //Loop 2 - "kn"
+    //Loop 3 - "kna"
+    //Loop 4 - "knar"
+    //Loop 5 - "knarF"
+    return revString
+
+}
+
+function displayString(revString) {
+    let output = document.getElementById("results");
+
+    output.innerHTML = revString;
 }
